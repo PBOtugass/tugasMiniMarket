3a81f0b1a8ae309809b6fde0e15c78efe9855356
laras825 <laras825@student.upi.edu>
2017-04-10 19:45:37 +0700

    masih in progress
    diterusin wani

--- TransaksiPembelian.java
+++ TransaksiPembelian.java
@@ -3,6 +3,9 @@ import java.util.HashMap;
 
 /**
  * Created by ASUS on 4/10/2017.
+ * Annisa Larasati / 1401947
+ * Warzuqni Olyan / 140
+ * Penddiikan Ilmu Komputer
  */
 public class TransaksiPembelian {
     int id_TransaksiP;
@@ -14,13 +17,6 @@ public class TransaksiPembelian {
     public void addBarang(Barang brg){ arrBarang.put(brg.id_barang,brg);}
     public void addSupplier(Suplier sup){ arrSuplier.put(sup.id_supplier,sup);}
 
-    public void print(){
-
-        for(Barang brg:arrBarang.values()){
-            brg.print();
-        }
-    }
-
     public Barang cariB(int barangId){
         return arrBarang.get(barangId);
     }
@@ -29,17 +25,32 @@ public class TransaksiPembelian {
         return arrSuplier.get(suplierId);
     }
 
-    public TransaksiPembelian(int id_TransaksiP, int id_staff, int id_supplier, int id_barang, String nama, String Jenis, int jumlah, int harga_suplier){
-    if(sup.id_supplier==id_supplier){
-        if(brg.id_barang==id_barang){
-            brg.upJumlahBarang(id_barang,jumlah);
+    public TransaksiPembelian(int id_TransaksiP, int id_staff, int id_supplier, String nama_supplier, String alamat_supplier, String kontak_supplier, int id_barang, String nama, String jenis, int jumlah, int harga_suplier, int harga_jual){
+        this.id_TransaksiP=id_TransaksiP;
+
+        if(sup.id_supplier==id_supplier){
+            if(brg.id_barang==id_barang){
+                brg.upJumlahBarang(id_barang,jumlah);
+            }else{
+                addBarang(new Barang(brg.id_barang = id_barang,brg.id_supplier, brg.nama_barang, brg.jenis=jenis, brg.jumlah=jumlah,brg.harga_suplier=harga_suplier,brg.harga_jual=harga_jual));
+            }
         }else{
-
+            addSupplier(new Suplier(sup.id_supplier=id_supplier,sup.nama_supplier=nama_supplier,sup.alamat_supplier=alamat_supplier,sup.kontak_supplier=kontak_supplier));
+            if(brg.id_barang==id_barang){
+                brg.upJumlahBarang(id_barang,jumlah);
+            }else{
+                addBarang(new Barang(brg.id_barang = id_barang,brg.id_supplier, brg.nama_barang, brg.jenis=jenis, brg.jumlah=jumlah,brg.harga_suplier=harga_suplier,brg.harga_jual=harga_jual));
+            }
         }
-    }else{
-
     }
-     //    addBarang(new Barang(brg.id_barang = id_barang,brg.nama_barang ));
+    public void print(){
+        for(Suplier sup:arrSuplier.values()){
+            for(Barang brg:arrBarang.values()){
+                if(sup.id_supplier==brg.id_supplier){
+
+                }
+            }
+        }
     }
 
 
