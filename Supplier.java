177f7dae114686d3399eee1d075e8e1c29b9230c
laras825 <laras825@student.upi.edu>
2017-05-02 15:36:18 +0700

    Supplier

---
+++ Supplier.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+/**
+ * Created by ASUS on 5/2/2017.
+ */
+public class Supplier {
+    ArrayList<Supplier> arrSupplier = new ArrayList<>();
+    public double hitungDiskon(){ return 0; }
+
+    public void print(){
+        System.out.println("-----Daftar Supplier----");
+        System.out.println("------------------------");
+        for(Supplier s:arrSupplier){
+            s.print();
+            System.out.println("");
+        }
+    }
+    public static void main(String[] args) {
+        Supplier s= new Supplier();
+
+        Umum s1 = new Umum(1,"Mayora","Bandung", "088345543888");
+        s.arrSupplier.add(s1);
+
+        Sponsor s2 = new Sponsor(2,"Wings","Bandung","083888789***");
+        s.arrSupplier.add(s2);
+
+        s.print();
+    }
+}
