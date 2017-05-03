98bc31b437ed22ac681844893ff0017f5d7c5bc5
laras825 <laras825@student.upi.edu>
2017-05-03 20:24:05 +0700

    Pembayaran Online
    Hisyam Yusyaq 1400494

---
+++ pembayaranonline.java
@@ -0,0 +1,27 @@
+package tubesv1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *Hisyam yusyaq 1400494
+ * @author ICAM
+ */
+public class pembayaranonline {
+    int id_barang;
+    int jumlah;
+    String harga;
+    String nama_barang;
+    
+    	public void print() {
+         System.out.println("id : "+id_barang);
+         System.out.println("Nama : "+nama_barang);
+         System.out.println("Jumlah : "+jumlah);
+         System.out.println("Harga : "+harga);
+	}
+        
+   
+    
+}
