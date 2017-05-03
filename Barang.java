2c110cca0652a40fc0fe3e43b8a7d2315c5d94e2
laras825 <laras825@student.upi.edu>
2017-05-03 20:36:12 +0700

    Barang
    Ade Kurniawan 1400650

--- Barang.java
+++ Barang.java
@@ -1,30 +1,33 @@
 /**
- * Created by ASUS on 4/9/2017.
- * Annisa Larasati / 1401947
- * Penddiikan Ilmu Komputer A
+ * Ade Kurniawan 1400650
+ * Created by Ade Kurniawan on 03/05/2017.
  */
+package tubesv1;
 public class Barang {
     int id_barang;
-    int id_supplier;
     String nama_barang;
     String jenis;
     int jumlah;
     int harga_suplier;
     int harga_jual;
+    int harga_total;
     int upHargaS;
     int upHargaJ;
     int upjumlah;
     int idUp;
+    int jumlahbeli;
+    int tampung;
 
-
-    public Barang(int id_barang, int id_Supplier,String nama_barang, String jenis, int jumlah, int harga_suplier, int harga_jual){
+    public Barang(int id_barang,String nama_barang, String jenis, int jumlah, int harga_suplier, int harga_jual,int harga_total,int jumlahbeli,int tampung){
         this.id_barang = id_barang;
-        this.id_supplier = id_Supplier;
         this.nama_barang = nama_barang;
         this.jenis = jenis;
         this.jumlah = jumlah;
         this.harga_suplier = harga_suplier;
         this.harga_jual = harga_jual;
+        this.harga_total = harga_total;
+        this.jumlahbeli = jumlahbeli;
+        this.tampung = tampung;
     }
 
     public int upHargaBarang(int idUp,int upHargaS,int upHargaJ){ //untuk mengupdate harga suplier dan harga jual barang
@@ -44,16 +47,25 @@ public class Barang {
 
     public void print(){
         System.out.println("Id barang :"+id_barang);
-        System.out.println("Id supplier :"+id_supplier);
         System.out.println("Nama Barang :"+nama_barang);
         System.out.println("Jenis Barang :"+jenis);
         System.out.println("Jumlah Barang :"+jumlah);
         System.out.println("Harga Suplier:"+harga_suplier);
         System.out.println("Harga jual:"+harga_jual);
+        System.out.println("total harga:"+harga_total);
+    }
+    
+     public void printT(){
+        System.out.println("Id barang :"+id_barang);
+        System.out.println("Nama Barang :"+nama_barang);
+        System.out.println("Harga jual:"+harga_jual);
+        System.out.println("Jumlah beli:"+jumlahbeli);
+        System.out.println("total harga:"+harga_total);
     }
 
+
     public static void main(String[] args){
-        Barang brg = new Barang(1,1,"indomie","mie",50, 2300, 2500);
+        Barang brg = new Barang(1,"indomie","mie",50, 2300, 2500,2500,0,0);
         brg.upHargaBarang(1,2500, 2800);
         System.out.println("==Testing print isi==");
         brg.print();
