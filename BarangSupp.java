7273a65b0cd8fc78106afe0e404fa84623c79c16
laras825 <laras825@student.upi.edu>
2017-05-03 21:17:38 +0700

    BarangSupp
    Ade Kurniawan 1400650

---
+++ BarangSupp.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Ade Kurniawan on 03/05/2017.
+ * Ade Kurniawan 1400650
+ */
+//package tubesv1;
+public class BarangSupp {
+    int id_barangsupp, harga, jumlah, total_harga;
+
+    public BarangSupp(int id_barangsupp, int harga, int jumlah , int total_harga){
+        this.id_barangsupp = id_barangsupp;
+        this.harga = harga;
+        this.jumlah = jumlah;
+        this.total_harga = total_harga;
+    }
+}
